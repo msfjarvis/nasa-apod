824655ddc9e1c4630cfcc0fa7bd19cb79fec61c0
Harsh Shandilya <me@msfjarvis.dev>
2020-11-13 19:16:41 +0530

    ui: load and store images in MainViewModel
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- ui/src/main/kotlin/dev/msfjarvis/apod/ui/main/MainViewModel.kt
+++ ui/src/main/kotlin/dev/msfjarvis/apod/ui/main/MainViewModel.kt
@@ -1,7 +1,23 @@
 package dev.msfjarvis.apod.ui.main
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.msfjarvis.apod.data.model.PictureDetail
+import dev.msfjarvis.apod.data.remote.ApodApi
+import dev.msfjarvis.apod.util.date.PictureDetailComparator
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-  // TODO: Implement the ViewModel
+class MainViewModel @ViewModelInject constructor(private val apodApi: ApodApi) : ViewModel() {
+
+  private val _images = MutableStateFlow<List<PictureDetail>>(emptyList())
+  val images: StateFlow<List<PictureDetail>> = _images
+
+  init {
+    viewModelScope.launch {
+      _images.value = apodApi.getImages().sortedWith(PictureDetailComparator)
+    }
+  }
 }
